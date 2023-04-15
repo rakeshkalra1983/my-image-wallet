52251f6d13db7c25cd90f593cfb4b11784a78b6b
TheGoodDude22 <61601241+TheGoodDude22@users.noreply.github.com>
2023-04-15 04:29:44 -0500

    cut to barebones

--- src/open-wallet.tsx
+++ src/open-wallet.tsx
@@ -1,5 +1,66 @@
-import Wallet from "./components/view-wallet"
+import { environment, Action, ActionPanel, Grid } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+
+import { fetchFiles } from "./utils";
+import { Card } from "./types";
+import { ReactNode } from "react";
 
 export default function Command() {
-	return <Wallet />
+	const { isLoading, data } = usePromise(loadGridComponents);
+
+	return (
+		<Grid
+			columns={5}
+			isLoading={isLoading}
+			inset={Grid.Inset.Large}
+			navigationTitle="Image Wallet"
+			searchBarPlaceholder="Search Cards..."
+			actions={
+				loadEditAction()
+			  }
+		>
+			{ data }
+		</Grid>
+	);
 }
+
+async function loadGridComponents() {
+	const outPockets:ReactNode[] = []
+
+	fetchFiles(environment.supportPath).then(pockets => {
+		pockets.forEach((pocket) => { outPockets.push(
+			<Grid.Section title={pocket.name} key={pocket.name || "unsorted"}>
+				{pocket.cards.map(card => (
+					<Grid.Item
+						key={card.path}
+						content={card.path}
+						title={card.name}
+						actions={loadCardActions(card)}
+					/>
+				))}
+			</Grid.Section>
+		)})
+	})
+
+	return outPockets
+}
+
+function loadCardActions(item: Card) { return (
+	<ActionPanel>
+		<ActionPanel.Section>
+			<Action.Paste content={{ file: item.path }} />
+			<Action.CopyToClipboard content={{ file: item.path }} />
+		</ActionPanel.Section>
+		{loadEditAction()}
+	</ActionPanel>
+)}
+
+function loadEditAction() { return (
+	<ActionPanel.Section>
+		<Action.ShowInFinder
+			title="Edit Wallet"
+			shortcut={{modifiers: ["cmd"], key: "e"}}
+			path={environment.supportPath}
+		/>
+	</ActionPanel.Section>
+)}
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
-import { confirmAlert, environment, Alert } from "@raycast/api";
+import { environment } from "@raycast/api";
 
 import { basename, extname } from "path";
 import { lstatSync, readdirSync } from "fs";
 
-import { Pocket, Card, CardForm } from "./types"
+import { Pocket, Card } from "./types"
 
 export async function fetchFiles(dir: string): Promise<Pocket[]> {
 	const pocketArr: Pocket[] = []
@@ -28,23 +28,6 @@ export async function fetchFiles(dir: string): Promise<Pocket[]> {
 		})
 	})
 
-/* 	readdir(environment.supportPath, (err, items: string[]) => {
-		items.forEach(item => {
-			const filePath = `${dir}/${item}`
-			const fileStats = lstatSync(filePath)
-			const fileExt = extname(filePath)
-			const fileName = basename(filePath, fileExt)
-
-			if (!fileStats.isDirectory()) return;
-			if (fileName.startsWith(".")) return;
-
-			loadPocketCards(`${dir}/${item}`)
-			.then(cards => {
-				pocketArr.push({ name: item, cards: cards})
-			})
-		})
-	}) */
-
 	return pocketArr;
 }
 
@@ -63,85 +46,6 @@ async function loadPocketCards(dir :string): Promise<Card[]> {
 
 		cardArr.push({ name: fileName, path: filePath });
 	})
-	/* readdir(dir, (err, items: string[]) => {
-		items.forEach(item => {
-			const filePath = `${dir}/${item}`;
-			const fileStats = lstatSync(filePath)
-			const fileExt = extname(filePath)
-			const fileName = basename(filePath, fileExt)
-
-			if (fileStats.isDirectory()) return;
-			if (fileName.startsWith(".")) return;
-
-			cardArr.push({ name: fileName, path: filePath });
-		})
-	}) */
 
 	return cardArr
-}
-
-/*
-export async function fetchFiles() {
-	const cardArr: Array<CardItem> = []
-
-	readdir(environment.supportPath, (err, topLevelItems: string[]) => {
-		topLevelItems.forEach(topLevelItem => {
-			const filePath = `${environment.supportPath}/${topLevelItem}`;
-			const fileStats = lstatSync(filePath)
-
-			if (fileStats.isDirectory()) {
-				const cardSubArr: Array<Array<CardItem> = []
-
-				readdir(environment.supportPath + topLevelItem, (err, subLevelItems: string[]) => {
-					subLevelItems.forEach(subLevelItem => {
-						const subFilePath = `${environment.supportPath}/${topLevelItem}/${subLevelItem}`
-						const subFileStats = lstatSync(subFilePath)
-						const subFileExt = extname(subFilePath)
-						const subFileName = basename(subFilePath, subFileExt)
-
-						if (subFileStats.isDirectory()) return;
-						cardSubArr.push({ name: subFileName, path: subFilePath, folder: topLevelItem})
-					})
-				})
-			} else {
-				const fileExt = extname(filePath)
-				const fileName = basename(filePath, fileExt)
-
-				if (fileName.startsWith(".")) return;
-				cardArr.push({ name: fileName, path: filePath });
-			}
-		});
-	});
-
-	return cardArr
-}
-*/
-
-export function createCard(values: CardForm) {
-	console.log(`Created ${values.name}`)
-}
-
-export function editCard(name: string) {
-	console.log(`Editing ${name}`);
-}
-export function deleteCard(name: string) {
-	console.log(`Deleted ${name}`);
-}
-export function deletePocket(pocket: string) {
-	console.log(`Deleted ${pocket}`);
-}
-export async function deleteAll(): Promise<void> {
-	const options: Alert.Options = {
-		title: "Delete All Cards",
-		icon: { source: "extension-icon.png" },
-		message: "Are you sure you want to delete all Cards?",
-		primaryAction: {
-			title: "Delete All",
-			style: Alert.ActionStyle.Destructive,
-		},
-	};
-
-	if (await confirmAlert(options)) {
-		console.log("Deleted all Cards.");
-	}
 }
