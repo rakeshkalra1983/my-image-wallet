fde3ee37f0e96c464c3494a69d33cee2971c1f84
TheGoodDude22 <61601241+TheGoodDude22@users.noreply.github.com>
2023-04-25 11:53:06 -0500

    Added reload action

--- src/open-wallet.tsx
+++ src/open-wallet.tsx
@@ -8,7 +8,7 @@ import { Card, Pocket } from "./types";
 
 export default function Command() {
 	const [pocket, setPocket] = useState<string>();
-	const { isLoading, data } = usePromise(loadGridComponents, [pocket]);
+	const { isLoading, data, revalidate} = usePromise(loadGridComponents, [pocket]);
 
 	return (
 		<Grid
@@ -37,86 +37,92 @@ export default function Command() {
 			{ data?.pocketNodes }
 		</Grid>
 	);
-}
 
-async function loadGridComponents(sortedPocket?:string) { return(
-	fetchFiles(walletPath).then(pockets => {
-		const dropdownNodes = loadGridDropdownNodes(pockets)
-		const pocketNodes:ReactNode[] = []
+	async function loadGridComponents(sortedPocket?:string) { return(
+		fetchFiles(walletPath).then(pockets => {
+			const dropdownNodes = loadGridDropdownNodes(pockets)
+			const pocketNodes:ReactNode[] = []
 
-		if (sortedPocket) {
-			pockets.forEach(pocket => {
-				if (pocket.name == sortedPocket) {
-					pocketNodes.push(loadPocketNodes(pocket, {hideTitle: true}));
-				}
-			})
-		} else {
-			pockets.forEach((pocket) => {
-				pocketNodes.push(loadPocketNodes(pocket));
-			})
-		}
+			if (sortedPocket) {
+				pockets.forEach(pocket => {
+					if (pocket.name == sortedPocket) {
+						pocketNodes.push(loadPocketNodes(pocket, {hideTitle: true}));
+					}
+				})
+			} else {
+				pockets.forEach((pocket) => {
+					pocketNodes.push(loadPocketNodes(pocket));
+				})
+			}
 
-		return { pocketNodes, dropdownNodes }
-	})
-)}
+			return { pocketNodes, dropdownNodes }
+		})
+	)}
 
-function loadGridDropdownNodes(pockets: Pocket[]) { return ([
-	<Grid.Dropdown.Item
-		title="All Cards"
-		value=""
-		key=""
-	/>,
-	<Grid.Dropdown.Section title="Pockets" key="Section">
-		{pockets.filter(pocket => (pocket.name))
-			.map(pocket => (
-				<Grid.Dropdown.Item
-					title={pocket.name || "Unsorted"}
-					value={pocket.name || "Unsorted"}
-					key={pocket.name || "Unsorted"}
+	function loadGridDropdownNodes(pockets: Pocket[]) { return ([
+		<Grid.Dropdown.Item
+			title="All Cards"
+			value=""
+			key=""
+		/>,
+		<Grid.Dropdown.Section title="Pockets" key="Section">
+			{pockets.filter(pocket => (pocket.name))
+				.map(pocket => (
+					<Grid.Dropdown.Item
+						title={pocket.name || "Unsorted"}
+						value={pocket.name || "Unsorted"}
+						key={pocket.name || "Unsorted"}
+					/>
+				))
+			}
+		</Grid.Dropdown.Section>
+	])}
+
+	function loadPocketNodes(pocket:Pocket, ops?:{ hideTitle?:boolean }) { return(
+		<Grid.Section
+			title={ ops?.hideTitle ? undefined : pocket.name || undefined }
+			key={pocket.name || "unsorted"}
+		>
+			{pocket.cards.map(card => (
+				<Grid.Item
+					key={card.path}
+					content={card.path}
+					title={card.name}
+					actions={loadCardActionNodes(card)}
 				/>
-			))
-		}
-	</Grid.Dropdown.Section>
-])}
+			))}
+		</Grid.Section>
+	)}
 
-function loadPocketNodes(pocket:Pocket, ops?:{ hideTitle?:boolean }) { return(
-	<Grid.Section
-		title={ ops?.hideTitle ? undefined : pocket.name || undefined }
-		key={pocket.name || "unsorted"}
-	>
-		{pocket.cards.map(card => (
-			<Grid.Item
-				key={card.path}
-				content={card.path}
-				title={card.name}
-				actions={loadCardActionNodes(card)}
-			/>
-		))}
-	</Grid.Section>
-)}
+	function loadCardActionNodes(item: Card) { return (
+		<ActionPanel>
+			<ActionPanel.Section>
+				<Action.Paste content={{ file: item.path }} />
+				<Action.CopyToClipboard content={{ file: item.path }} />
+			</ActionPanel.Section>
+			{loadEditActionNodes()}
+		</ActionPanel>
+	)}
 
-function loadCardActionNodes(item: Card) { return (
-	<ActionPanel>
+	function loadEditActionNodes() { return (
 		<ActionPanel.Section>
-			<Action.Paste content={{ file: item.path }} />
-			<Action.CopyToClipboard content={{ file: item.path }} />
+			<Action
+				title="Reload"
+				icon={Icon.ArrowClockwise}
+				shortcut={{ modifiers: ["cmd"], key: "r" }}
+				onAction={revalidate}
+			/>
+			<Action.ShowInFinder
+				title="Open Wallet in Finder"
+				shortcut={{ modifiers: ["cmd"], key: "e" }}
+				path={walletPath}
+			/>
+			<Action
+				title="Change Wallet Directory"
+				icon={Icon.Folder}
+				shortcut={{ modifiers: ["cmd", "shift"], key: "e" }}
+				onAction={openExtensionPreferences}
+			/>
 		</ActionPanel.Section>
-		{loadEditActionNodes()}
-	</ActionPanel>
-)}
-
-function loadEditActionNodes() { return (
-	<ActionPanel.Section>
-		<Action.ShowInFinder
-			title="Open Wallet in Finder"
-			shortcut={{ modifiers: ["cmd"], key: "e" }}
-			path={walletPath}
-		/>
-		<Action
-			title="Change Wallet Directory"
-			icon={Icon.Folder}
-			shortcut={{ modifiers: ["cmd", "shift"], key: "e" }}
-			onAction={openExtensionPreferences}
-		/>
-	</ActionPanel.Section>
-)}
+	)}
+}
